451fd3da1e03735604c1da2ffbd1e14a16ca5738
NicholasPapazis <nicholasepapazis@gmail.com>
2024-08-29 10:27:18 -0400

    Added comment to hello world statement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+        //displays hello world text
         System.out.println("Hello world!");
+
     }
 }
