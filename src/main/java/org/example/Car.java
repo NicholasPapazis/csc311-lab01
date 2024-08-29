f5cf98d203ca5aed1a900e77d6f6f0e3e0a199fb
NicholasPapazis <nicholasepapazis@gmail.com>
2024-08-29 11:18:44 -0400

    created display info method

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -11,9 +11,38 @@ public class Car extends Vehicle {
         this.brand = brand;
     }
 
-    //honk function
+    //getter for brand
+    public String getBrand() {
+        return brand;
+    }
+
+    //setter for brand
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    //honk method
     public void honk() {
         System.out.println("Honk, honk!");
+    }
+
+    //method that displays info
+    public void displayInfo() {
+        int numWheels = super.getNumberOfWheels();
+        String carColor = super.getColor();
+        float engine = super.getEngineSize();
+        String fuel = super.getFuelType();
+        String carBrand = brand;
+
+        System.out.println(
+                "car info:" + '\n' +
+                "number of wheels: " + numWheels + "\n" +
+                "color: " + numWheels + "\n" +
+                "engine size: " + numWheels + "\n" +
+                "fuel type: " + numWheels + "\n" +
+                "brand: " + numWheels + "\n"
+        );
+
     }
 
 
