4aad4f3a8780645a3e38076ab1ac3961e7484e5e
NicholasPapazis <nicholasepapazis@gmail.com>
2024-08-29 10:41:20 -0400

    fixed mistake by moving attributes from Car class to Vehicle class

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -2,11 +2,7 @@ package org.example;
 
 public class Car extends Vehicle {
 
-    //attributes for Vehicle
-    private int numberOfWheels;
-    private String color;
-    private float engineSize;
-    private String fuelType;
+
 
 
 
--- src/main/java/org/example/Vehicle.java
+++ src/main/java/org/example/Vehicle.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Vehicle {
+
+    //attributes for Vehicle
+    private int numberOfWheels;
+    private String color;
+    private float engineSize;
+    private String fuelType;
 }
