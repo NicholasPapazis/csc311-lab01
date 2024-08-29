2719a6365ffea60fd0393cfa6ecf12dbe47e4862
NicholasPapazis <nicholasepapazis@gmail.com>
2024-08-29 10:37:29 -0400

    Added attributes for Vehicle

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Car extends Vehicle {
+
+    //attributes for Vehicle
+    private int numberOfWheels;
+    private String color;
+    private float engineSize;
+    private String fuelType;
+
+
+
+
+}
