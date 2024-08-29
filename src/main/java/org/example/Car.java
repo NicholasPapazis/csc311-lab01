777a29201e4f840ce026efad25ff1d74a6649c52
NicholasPapazis <nicholasepapazis@gmail.com>
2024-08-29 11:01:34 -0400

    created constructor for Car

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -5,6 +5,14 @@ public class Car extends Vehicle {
     //additional attributes for Car
     private String brand;
 
+    //create constructor that sets additional attribute
+    public Car(int numberOfWheels, String color, float engineSize, String fuelType, String brand) {
+        super(numberOfWheels, color, engineSize, fuelType);
+        this.brand = brand;
+    }
+
+
+
 
 
 
